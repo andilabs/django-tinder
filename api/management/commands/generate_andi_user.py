e59970edede9b52246027eccec62480d9f398e7d
andrzej.kostanski <andrzej.kostanski@daftcode.pl>
2014-12-10 17:52:46 +0100

    added file for generating benchmark andi user

---
+++ api/management/commands/generate_andi_user.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from django.contrib.gis.geos import Point
+
+from api.models import FuckFinderUser
+
+
+class Command(BaseCommand):
+    help = 'generate andi user'
+
+    def handle(self, *args, **options):
+        FuckFinderUser.objects.create(
+            nickname='andi',
+            age=26,
+            sex='M',
+            prefered_sex='F',
+            prefered_age_min=21,
+            prefered_age_max=29,
+            last_location=Point(float(52.228625), float(21.001952)),
+            prefered_radius=5
+        )
